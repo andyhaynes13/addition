bb21b7c80b6f00818e3a58c5779dfac2040eb5f4
andyhaynes13 <admin@haynescomputerservice.com>
2017-07-12 11:31:31 -0400

    added variables

--- addition.rb
+++ addition.rb
@@ -2,6 +2,11 @@ x = 1 + 1
 y = 2 + 2
 z = 3 + 3
 
+def add(a,b)
+	puts "adding #{a}+#{b}"
+	return a + b
+end
+
 answer = x + y
 
 
@@ -12,3 +17,7 @@ p "2 + 2 =#{y}"
 p "3 + 3 = #{z}"
 
 p "#{x} + #{y} =  #{answer}"
+
+p add(6,7)
+
+p add(34,98)
